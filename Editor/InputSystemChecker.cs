83615a5fbca21832a24c95963ed6901e3d4b32b6
Marcello Tridenti <marcello.tridenti@tutanota.com>
2021-05-28 16:57:59 +0200

    Updated input system checker message

--- Editor/InputSystemChecker.cs
+++ Editor/InputSystemChecker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using VPG.Core.Input;
 using UnityEditor;
 
@@ -6,7 +6,7 @@ using UnityEditor;
 public static class InputSystemChecker
 {
     private const string message =
-        "From Creator version 2.11 onwards Unity's new Input System is required." +
+        "VR Process Gizmo requires Unity's new Input System." +
         "\n\nTo switch from the legacy input system to the new one, open the 'Player Settings' and set the " +
         "option 'Active Input Handling' to 'Both' or 'Input System Package (New)'.";
 
